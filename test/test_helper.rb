4ebe705c8801bd7a3176c08826ff33a874989422
Stephane Bounmy <stephanebounmy@gmail.com>
2013-09-25 01:07:33 +0200

    removed database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,8 +16,7 @@ require 'spree/testing_support/factories'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require 'capybara/rails'
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
+
 Capybara.default_driver = :selenium
 
 class ActionDispatch::IntegrationTest
@@ -26,10 +25,9 @@ class ActionDispatch::IntegrationTest
   include Spree::UrlHelpers
 
   # Stop ActiveRecord from wrapping tests in transactions
-  self.use_transactional_fixtures = false
+  self.use_transactional_fixtures = true
 
   teardown do
-    DatabaseCleaner.clean       # Truncate the database
     Capybara.reset_sessions!    # Forget the (simulated) browser state
     Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
   end
