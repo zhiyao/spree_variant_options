92c41f24246e0590e6ac78fc3e787f0ccfd54d10
Stephane Bounmy <stephanebounmy@gmail.com>
2013-09-25 21:15:58 +0200

    fix preferences spec

--- test/support/preferences.rb
+++ test/support/preferences.rb
@@ -1,13 +1,13 @@
-def reset_spree_preferences
+def reset_spree_variant_options_preferences(&config_block)
   Spree::Preferences::Store.instance.persistence = false
-  config = Rails.application.config.spree.preferences
-  config.reset
+  Spree::Preferences::Store.instance.clear_cache
+
+  config = Rails.application.config.spree.variant_preferences
+  configure_spree_variant_options_preferences &config_block if block_given?
+end
+
+def configure_spree_variant_options_preferences
+  config = Rails.application.config.spree.variant_preferences
   yield(config) if block_given?
 end
 
-def reset_spree_variant_options_preferences
-  Spree::Preferences::Store.instance.persistence = false
-  config = Rails.application.config.spree.variant_preferences
-  config.reset
-  yield(config) if block_given?
-end
