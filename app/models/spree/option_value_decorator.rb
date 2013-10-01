1d580f35148672c3b06e37dad272197b9fa7314f
Tiago Amaro <tiago.amaro@rastre.me>
2013-10-01 13:56:47 -0300

    removing attr_accessors for rails 4

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,7 +1,5 @@
 Spree::OptionValue.class_eval do
 
-  attr_accessible :image
-
   has_attached_file :image,
     :styles        => ActiveSupport::JSON.decode(Spree::Config[:attachment_styles]).symbolize_keys!,
     :default_style => SpreeVariantOptions::VariantConfig[:option_value_default_style],
@@ -11,11 +9,4 @@ Spree::OptionValue.class_eval do
   def has_image?
     image_file_name && !image_file_name.empty?
   end
-
-  scope :for_product, lambda { |product|
-    uniq.where("spree_option_values_variants.variant_id IN (?)", product.variant_ids)
-    .joins(:variants).order_by_positions
-  }
-
-  scope :order_by_positions, joins(:option_type).order("#{Spree::OptionType.quoted_table_name}.position asc, #{quoted_table_name}.position asc")
 end
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,8 +2,6 @@ Spree::Variant.class_eval do
 
   include ActionView::Helpers::NumberHelper
 
-  attr_accessible :option_values
-
   def to_hash
     actual_price  = self.price
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
