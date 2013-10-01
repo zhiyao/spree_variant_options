493980506163b79d377f33fc74cb50df0e0a7143
Tiago Amaro <tiago.amaro@rastre.me>
2013-10-01 14:36:37 -0300

    removing default scope deprecation warning

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -10,6 +10,6 @@ Spree::OptionValue.class_eval do
     image_file_name && !image_file_name.empty?
   end
 
-  default_scope order("#{quoted_table_name}.position")
+  default_scope { order("#{quoted_table_name}.position") }
   scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_values_variants.variant_id IN (?)", product.variant_ids).joins(:variants) }
 end
