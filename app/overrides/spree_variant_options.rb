fe35f2abdcf89970e91579da1149226146f39689
Tiago Amaro <tiago.amaro@rastre.me>
2013-11-04 12:49:39 -0200

    adding view and specs for checkbox upload on admin area

--- app/overrides/spree_variant_options.rb
+++ app/overrides/spree_variant_options.rb
@@ -15,3 +15,10 @@ Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
                      :set_attributes => "tr[data-hook=option_none] td",
                      :attributes     => { :colspan => 5 },
                      :disabled       => false)
+
+# Image upload for many variants on admin area
+Deface::Override.new(:virtual_path   => "spree/admin/images/form",
+                     :name           => "admin_multiple_image_upload_for_variant_options",
+                     :replace        => "[data-hook=admin_image_form_fields]",
+                     :partial        => "spree/admin/images/form",
+                     :disabled       => false)
--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -1,19 +1,20 @@
 module SpreeVariantOptions
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      
+
       desc "Installs required migrations for spree_essentials"
-      
+
       def copy_migrations
         rake "spree_variant_options:install:migrations"
       end
-      
+
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require spree_variant_options\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", "*= require spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/store/all.css", "*= require store/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/admin/all.css", "*= require admin/variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
